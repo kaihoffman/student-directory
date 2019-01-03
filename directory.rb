6ded7dd892d1707990df1116b3951c4a264eee1b
Kai Hoffman <notkaiho@gmail.com>
2019-01-03 13:53:07 +0000

    Exercise 1

--- directory.rb
+++ directory.rb
@@ -92,8 +92,8 @@ def print_header
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  @students.each_with_index do |student, index|
+    puts "#{index + 1 }. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
   end
 end
 
